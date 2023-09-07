dee60da2f9ad373de655a5ef23518904a663ddf9
KASUNHapangama <88395585+KASUNHapangama@users.noreply.github.com>
2023-09-07 17:55:56 +0530

    - added SSL support

--- src/main/java/org/kasun/website/Server/WebServer.java
+++ src/main/java/org/kasun/website/Server/WebServer.java
@@ -1,36 +1,72 @@
 package org.kasun.website.Server;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.*;
 import org.bukkit.plugin.Plugin;
 import org.kasun.website.SimpleWebsite;
 import org.kasun.website.Utils.FileUtils;
 
+import javax.net.ssl.*;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.KeyStore;
 
 public class WebServer {
     private HttpServer server;
+    private HttpsServer httpsServer;
     private Plugin plugin;
     private String name;
     private String websiteFolder;
     private static WebServer instance;
     private int port;
+    private boolean useSSL;
 
-    public WebServer(String websiteFolder, int port) {
+    public WebServer(String websiteFolder, int port, boolean useSSL) {
         this.plugin = SimpleWebsite.getInstance();
         this.name = name;
         this.websiteFolder = websiteFolder;
         this.port = port;
+        this.useSSL = useSSL;
         instance = this;
-    }
-    public void start() {
 
+        if (useSSL) {
+            setupSSL();
+        }
+    }
+
+    private void setupSSL() {
+        try {
+            // Load your keystore here (replace "keystore.p12" and "keystore_password" with your values)
+            char[] keystorePassword = "test".toCharArray();
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            keyStore.load(new FileInputStream(plugin.getDataFolder() + "/keystore.p12"), keystorePassword);
+
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            keyManagerFactory.init(keyStore, keystorePassword);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+
+            // Create HTTPS context and configurator
+            HttpsConfigurator configurator = new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    SSLParameters sslParameters = getSSLContext().getDefaultSSLParameters();
+                    params.setSSLParameters(sslParameters);
+                }
+            };
+
+            httpsServer = HttpsServer.create(new InetSocketAddress(port), 0);
+            httpsServer.setHttpsConfigurator(configurator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
         // Check if the example HTML file needs to be created
         File exampleHtml = new File(plugin.getDataFolder() + "/public/" + websiteFolder + "/index.html");
 
@@ -46,23 +82,34 @@ public class WebServer {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
 
+        // Set up the HTTP/HTTPS server
+        if(useSSL) {
+
+            httpsServer.createContext("/", new WebsiteHandler(websiteFolder));
+            httpsServer.setExecutor(null); // Use the default executor
+            httpsServer.start();
+
+        } else {
+            try {
+                server = HttpServer.create(new InetSocketAddress(port), 0);
+                server.createContext("/", new WebsiteHandler(websiteFolder));
+                server.setExecutor(null); // Use the default executor
+                server.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        // Set up the HTTP server
-        try {
-            server = HttpServer.create(new InetSocketAddress(port), 0);
-            server.createContext("/", new WebsiteHandler(websiteFolder));
-            server.setExecutor(null); // Use the default executor
-            server.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 
     public void stop() {
-
         if (server != null) {
             server.stop(0);
+        }
+        if (httpsServer != null) {
+            httpsServer.stop(0);
         }
     }
 
@@ -101,3 +148,8 @@ public class WebServer {
         }
     }
 }
+
+
+
+
+
--- src/main/java/org/kasun/website/Server/WebServerManager.java
+++ src/main/java/org/kasun/website/Server/WebServerManager.java
@@ -3,9 +3,10 @@ package org.kasun.website.Server;
 public class WebServerManager {
     public WebServerManager(){
         //start web server
-        WebServer webServer = new WebServer("default", 8081);
+
+        WebServer webServer = new WebServer("default", 8081, true);
         webServer.start();
-        WebServer webServer2 = new WebServer("web2", 8082);
+        WebServer webServer2 = new WebServer("web2", 8082, false);
         webServer2.start();
     }
 
